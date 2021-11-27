debe72cb467fa9dc78c2b3129fc1c5675f2f3179
BillyDungeonMaster <93702538+BillyDungeonMaster@users.noreply.github.com>
2021-11-27 23:04:41 +0800

    Update MainActivity.java

--- CalculatorByXZ/app/src/main/java/com/example/calculatorbyxz/MainActivity.java
+++ CalculatorByXZ/app/src/main/java/com/example/calculatorbyxz/MainActivity.java
@@ -8,10 +8,6 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     static TextView first_value, second_value, result;
-    public void PlusButtonClick(View view){
-        int n = Integer.parseInt(first_value.toString()), m = Integer.parseInt(second_value.toString());
-        result.setText(String.valueOf(n + m));
-    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,5 +15,49 @@ public class MainActivity extends AppCompatActivity {
         first_value = findViewById(R.id.firstvalue);
         second_value = findViewById(R.id.secondvalue);
         result = findViewById(R.id.result);
+    }
+    public static boolean isDigit(String s) throws NumberFormatException {
+        try {
+            Double.parseDouble(s);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+    public void PlusButtonClick(View view){
+        if(isDigit(String.valueOf(first_value.getText())) && isDigit(String.valueOf(second_value.getText()))){
+            double n = Double.parseDouble(String.valueOf(first_value.getText())), m = Double.parseDouble(String.valueOf(second_value.getText()));
+            result.setText(String.valueOf(n + m));
+        }
+        else{
+            result.setText("Error! Value incorrect.");
+        }
+    }
+    public void MinusButtonClick(View view){
+        if(isDigit(String.valueOf(first_value.getText())) && isDigit(String.valueOf(second_value.getText()))){
+            double n = Double.parseDouble(String.valueOf(first_value.getText())), m = Double.parseDouble(String.valueOf(second_value.getText()));
+            result.setText(String.valueOf(n - m));
+        }
+        else{
+            result.setText("Error! Value incorrect.");
+        }
+    }
+    public void DivideButtonClick(View view){
+        if(isDigit(String.valueOf(first_value.getText())) && isDigit(String.valueOf(second_value.getText()))){
+            double n = Double.parseDouble(String.valueOf(first_value.getText())), m = Double.parseDouble(String.valueOf(second_value.getText()));
+            result.setText(String.valueOf(n / m));
+        }
+        else{
+            result.setText("Error! Value incorrect.");
+        }
+    }
+    public void ScaleButtonClick(View view){
+        if(isDigit(String.valueOf(first_value.getText())) && isDigit(String.valueOf(second_value.getText()))){
+            double n = Double.parseDouble(String.valueOf(first_value.getText())), m = Double.parseDouble(String.valueOf(second_value.getText()));
+            result.setText(String.valueOf(n * m));
+        }
+        else{
+            result.setText("Error! Value incorrect.");
+        }
     }
 }
