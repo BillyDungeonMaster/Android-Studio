35045ff5ebad84b54dcb05d14980d77928f78c71
BillyDungeonMaster <93702538+BillyDungeonMaster@users.noreply.github.com>
2021-11-27 22:41:40 +0800

    Update MainActivity.java

--- CalculatorByXZ/app/src/main/java/com/example/calculatorbyxz/MainActivity.java
+++ CalculatorByXZ/app/src/main/java/com/example/calculatorbyxz/MainActivity.java
@@ -7,15 +7,17 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView firstvalue = findViewById(R.id.firstvalue), secondvalue = findViewById(R.id.secondvalue), result = findViewById(R.id.result);
+    static TextView first_value, second_value, result;
     public void PlusButtonClick(View view){
-        String a = firstvalue.toString(), b = secondvalue.toString();
-        int n = Integer.parseInt(a), m = Integer.parseInt(b);
-        result.setText(n + m);
+        int n = Integer.parseInt(first_value.toString()), m = Integer.parseInt(second_value.toString());
+        result.setText(String.valueOf(n + m));
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        first_value = findViewById(R.id.firstvalue);
+        second_value = findViewById(R.id.secondvalue);
+        result = findViewById(R.id.result);
     }
 }
